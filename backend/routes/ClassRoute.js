5834e476aba427446c5483295361775c692d437c
nicklin-sjsu <nicklin1219@gmail.com>
2023-03-28 23:17:24 -0700

    add addactivity and addclass routes

--- backend/routes/ClassRoute.js
+++ backend/routes/ClassRoute.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const {
-    getAllClasses, getClassById, getClassesByGym
+    getAllClasses, getClassById, getClassesByGym, addActivity, addClass
 } = require("../controllers/ClassController");
 
 router.get("/getAllClasses", getAllClasses);
 router.get("/getClassById", getClassById);
 router.get("/getClassesByGym", getClassesByGym);
+router.get("/addActivity", addActivity);
+router.get("/addClass", addClass);
 
 module.exports = router;
