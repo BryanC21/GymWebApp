e30f2b94ec84b2c1adaab25c6d3ed2b11dff6cb0
nicklin-sjsu <nicklin1219@gmail.com>
2023-05-04 13:22:29 -0700

    Add get classes by user id

--- backend/routes/ClassRoute.js
+++ backend/routes/ClassRoute.js
@@ -2,7 +2,7 @@ const express = require("express");
 const employeeAuth = require("../controllers/auth/employeeAuth");
 const userAuth = require("../controllers/auth/userAuth");
 const {
-    getAllClasses, getClassById, getClassesByGym, addActivity, addClass, enrollClass
+    getAllClasses, getClassById, getClassesByGym, addActivity, addClass, enrollClass, getClassesByUserId
 } = require("../controllers/ClassController");
 
 const router = express.Router();
@@ -10,8 +10,9 @@ const router = express.Router();
 router.get("/getAllClasses", getAllClasses);
 router.get("/getClassById", getClassById);
 router.get("/getClassesByGym", getClassesByGym);
-router.get("/addActivity", addActivity);
-router.get("/addClass", addClass);
-router.get("/enrollClass", enrollClass);
+router.get("/addActivity", employeeAuth, addActivity);
+router.get("/addClass", employeeAuth, addClass);
+router.get("/enrollClass", userAuth, enrollClass);
+router.get("/getClassesByUserId", userAuth, getClassesByUserId);
 
 module.exports = router;
